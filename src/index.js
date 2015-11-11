533ae37a537f78e94ada1c417470668627631e40
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2015-11-12 01:12:05 +0800

    Firebase + re-base
    added db server for chat app

--- src/index.js
+++ src/index.js
@@ -1,31 +1,133 @@
-var dataSetter = require('./dataSet.js');
-var dataSet = dataSetter();
+// use re-base to connect firebase
+const Rebase = require('re-base');
+const base = Rebase.createClass('https://popping-fire-7829.firebaseio.com');
+
+// use modules
 var person = require('./person.js');
 
 const ENTER_KEY = 13;
 
 class App extends React.Component {
+
+  // Constructor
 	constructor(props) {
     	super(props);
-    	this.state = {statement: 0,
-    		value: ''};
+    	this.state = {
+        statement: 0,
+        messageHis: '',
+    		value: '',
+        Adam: '',
+        Betty: '',
+        Chris: '',
+      };
   	}
 
+  // re-base
+  componentDidMount() {
+    this.ref = [
+      base.syncState('Adam', {
+        context: this,
+        state: 'Adam',
+        asArray: false
+      }),
+      base.syncState('Betty', {
+        context: this,
+        state: 'Betty',
+        asArray: false
+      }),
+      base.syncState('Chris', {
+        context: this,
+        state: 'Chris',
+        asArray: false
+      }),
+    ]
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref[0]);
+    base.removeBinding(this.ref[1]);
+    base.removeBinding(this.ref[2]);
+  }
+
+  // action functions
  	clicks(st) {
 		this.setState({
 			statement:st,
 		});
+    switch(st){
+      case 1:
+        this.setState({
+          messageHis: this.state.Adam,
+        });
+      break;
+      case 2:
+        this.setState({
+          messageHis: this.state.Betty,
+        });
+      break;
+      case 3:
+        this.setState({
+          messageHis: this.state.Chris,
+        });
+      break;
+    }
 	}
 
 	handleChange(changeEvent) {
     	this.setState({value: changeEvent.target.value});
-  	}
+  }
 
-  	buttonOnclick(){
-    	if(this.state.value !== '')
-    		dataSet.setMessage(this.state.value,this.state.statement);
-    	this.setState({value :''});
-  	}
+  buttonOnclick(){
+    	if(this.state.value !== ''){
+        switch(this.state.statement){
+          case 1:
+            this.setState({
+              Adam : this.state.Adam + '\nme: ' + this.state.value,
+              messageHis : this.state.Adam + '\nme: ' + this.state.value,
+            });
+          break;
+          case 2:
+            this.setState({
+              Betty : this.state.Betty + '\nme: ' + this.state.value,
+              messageHis : this.state.Betty + '\nme: ' + this.state.value,
+            });
+          break;
+          case 3:
+            this.setState({
+              Chris : this.state.Chris + '\nme: ' + this.state.value,
+              messageHis : this.state.Chris + '\nme: ' + this.state.value,
+            });
+          break;
+          default:
+            this.setState({
+              value :''
+            });
+          break;
+        }
+      }
+      this.setState({
+        value: ''
+      });
+  }
+
+  getMessage(){
+    var toReturn = '';
+    switch(this.state.statement){
+      case 1:
+        toReturn = this.state.Adam;
+      break;
+      case 2:
+        toReturn =  this.state.Betty;
+      break;
+      case 3:
+        rtoReturn =  this.state.Chris;
+      break;
+      default:
+        toReturn = '';
+      break;
+    }
+    return toReturn;
+  }
 
 	render() {
     	return (
@@ -36,12 +138,12 @@ class App extends React.Component {
       			   <person onClick = {this.clicks.bind(this,3)} className = 'btn btn-default'>Chris</person>
                 </div>
       			
-      			<textarea rows="10" cols="50" value = {dataSet.getMessage(this.state.statement)}>
+      			<textarea rows="10" cols="50" value = {this.state.messageHis}>
       			</textarea>
       			
       			<div>
-					<input onChange = {this.handleChange.bind(this)} value = {this.state.value}></input>
-              		<button onClick = {this.buttonOnclick.bind(this)} className = 'btn btn-success'>submit</button>
+					     <input onChange = {this.handleChange.bind(this)} value = {this.state.value}></input>
+               <button onClick = {this.buttonOnclick.bind(this)} className = 'btn btn-success'>submit</button>
       			</div>
       		</div>
     	);
