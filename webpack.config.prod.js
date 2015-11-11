621b31ea9a88155f1dcfb681ab386cd8cdeab2b6
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2015-11-12 01:47:50 +0800

    list destination changed
    as title

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/Assignment5/dist/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
